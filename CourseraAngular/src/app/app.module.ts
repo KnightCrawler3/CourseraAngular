3fe18c492b56cc32d5f71726d204ad1ef91803df
KnightCrawler3 <manishbarasker@gmail.com>
2020-04-19 18:40:01 +0530

    sixth commit

--- CourseraAngular/src/app/app.module.ts
+++ CourseraAngular/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailsComponent
   ],
   imports: [
     BrowserModule,
