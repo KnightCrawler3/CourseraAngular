6022cdb96f142d654c5b8d92bf1a5b8d5d121cf9
KnightCrawler3 <manishbarasker@gmail.com>
2020-04-26 17:06:27 +0530

    reactive forms added

--- CourseraAngular/src/app/app.module.ts
+++ CourseraAngular/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing/app-routing.module'
 import { MatDialogModule, MatCheckboxModule } from '@angular/material'
 import { MatFormFieldModule } from '@angular/material'
-import { MatInputModule } from '@angular/material'
+import { MatInputModule, MatSelectModule , MatSlideToggleModule} from '@angular/material'
 import 'hammerjs';
 
 import { DishService } from './services/dish.service';
@@ -27,6 +27,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -53,7 +54,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule,MatCheckboxModule
+    MatInputModule,MatCheckboxModule,ReactiveFormsModule,
+    MatSelectModule,MatSlideToggleModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [
